56f452a7fdb2faef9a2c78b7c1409abd691be167
Joseph McGregor <joseph00@my.yorku.ca>
2021-11-13 18:00:22 -0500

    updates to weather scenarios

--- lighthouse-js-fundamentals/conditional.js
+++ lighthouse-js-fundamentals/conditional.js
@@ -1,5 +1,5 @@
-const raining = true;
-const cold = false;
+const raining = false;
+const cold = true;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
@@ -7,6 +7,19 @@ if (raining) {
 
 if (cold) {
   console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
