155a55b7bdf4e9f4a950aadef814d36703b238f2
Joseph McGregor <joseph00@my.yorku.ca>
2022-01-18 19:04:55 -0500

    Co-authored-by: Vivian Trinh <Vptrinh@users.noreply.github.com>

--- w2/d2/matrix_transponse(potential solution).js
+++ w2/d2/matrix_transponse(potential solution).js
@@ -1,13 +1,59 @@
-const transpose = function(matrix) {
-  // declare an empty array
+const transpose = function (matrix) {
   let array = [];
   for (let x = 0; x < matrix.length; x++) {
-    for (let y = 0; y < matrix[x].length; y++) {
+    for (let y = 0; y < matrix[x].length; y++)
       if (x === 0) {
-        array.push([matrix[x][y]]);
+        array.push([matrix[x][y]]); //[1,2,3,4] -- > [[1],[2],[3],[4]]
       } else {
         array[y].push(matrix[x][y]);
       }
-    }
   }
   return array;
+};
+
+
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write('\n')
+  }
+}
+
+printMatrix(transpose([
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4]
+]));
+
+// console.log('----')
+
+printMatrix(transpose([
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]));
+
+// console.log('----')
+
+printMatrix(transpose([
+  [1, 2, 3, 4, 5, 6, 7]
+]));
+// // Expected Logs
+// //   1 1 1 1
+// //   2 2 2 2
+// //   3 3 3 3
+// //   4 4 4 4
+// //   ----
+// //     1 3 5
+// //   2 4 6
+// //   ----
+// //     1
+// //   2
+// //   3
+// //   4
+// //   5
+// //   6
+// //   7
